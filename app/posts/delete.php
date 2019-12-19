3d0e1de07ae76c9dd580c0a742d649b60344c676
Victor Ljungblad <victor.ljungblad@gmail.com>
2019-12-19 14:19:31 +0100

    Added delete form

--- app/posts/delete.php
+++ app/posts/delete.php
@@ -4,7 +4,18 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
-if (isLoggedIn() && isset($_POST['id'])) {
-    // ALWAYS CHECK AGAINS THE USER WHEN DELETING OR CHANGING STUFF
-    // DELETE FROM users WHERE id = :id AND user_id = :user_id;
+if (!isLoggedIn()) {
+    redirect('/');
+}
+
+// DELETE FROM users WHERE id = :id AND user_id = :user_id;
+
+//TODO: ADD FUNCTION THAT CHECKS THE OWNER OF THE POST WITH THE ID
+
+if (isset($_GET['post_id'], $_POST['password'])) {
+    $userId = $_SESSION['user']['id'];
+    $postId = (int) filter_var($_GET['post_id'], FILTER_SANITIZE_NUMBER_INT);
+    $password = $_POST['password'];
+
+    //TODO: IMPLEMENT REST OF THE SCRIPT
 }
--- edit-post.php
+++ edit-post.php
@@ -10,7 +10,9 @@ $post = getPostById($_GET['post_id'], $user['id'], $pdo);
     <h1>Edit post</h1>
     <?php require __DIR__.'/views/error.php'; ?>
     <?php require __DIR__.'/views/success.php'; ?>
+</article>
 
+<article>
     <form class="edit-post-form" action="<?php echo 'app/posts/update.php?post_id='.$post['post_id']; ?>" method="POST" enctype="multipart/form-data">
         <img src="<?php echo '/uploads/'.$post['image']; ?>" alt="">
         <label for="edit_post_image">Edit image</label>
@@ -18,6 +20,15 @@ $post = getPostById($_GET['post_id'], $user['id'], $pdo);
         <label for="edit_post_content">Edit content</label>
         <textarea type="text" name="edit_post_content" rows="8" cols="40"><?php echo $post['content']; ?></textarea>
         <button type="submit">Update post</button>
+    </form>
+</article>
+
+<article>
+    <h2>Delete post</h2>
+    <form action="<?php echo 'app/posts/delete.php?post_id='.$post['post_id']; ?>" method="POST">
+        <label for="password">Enter your password to delete this post</label>
+        <input type="password" name="password" placeholder="Password" required>
+        <button type="submit">Delete post</button>
     </form>
 </article>
 
